7bf2ffbb5956b0b3d82b56a3c3b43a38984add28
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-07-14 18:48:20 -0300

    final commit

--- anagrams1/anagrams1.js
+++ anagrams1/anagrams1.js
@@ -1,18 +1,42 @@
-const button = document.getElementById("findButton");
+let body = document.querySelector('body')
+let h1 = document.createElement('h1')
+h1.innerHTML = "Anagramas"
+body.appendChild(h1)
+let div = document.createElement("div")
+let inputHtml = document.createElement("input")
+inputHtml.type = "text"
+inputHtml.id = "input"
+let button = document.createElement("button")
+button.id = "findButton"
+button.innerText = "Encontrar Anagramas"
+div.append(inputHtml, button)
+let p = document.createElement("p")
+body.append(div, p)
 
-//capturando evento de click no botão
-    button.addEventListener("click", function () {
+button.addEventListener("click", function () {
         let typedText = document.getElementById("input").value;
-        getAnagramsOf(typedText);
+        getAnagramsOf(typedText)
 });
 
-//encontrar todas as palavras que fazem parte do anagrama
-function getAnagramsOf(input){
+function getAnagramsOf(input){ 
+    let inputAlterado = alphabetize(input)
+    let arrayStrings = []
 
+    for(let j = 0; j < palavras.length; j++){
+    let palavraAlterada = alphabetize(palavras[j])
+    if(inputAlterado == palavraAlterada){
+        arrayStrings.push(palavras[j])
+    }
+    }
+    for(let i = 0; i < arrayStrings.length; i++){
+        p.innerText = `${arrayStrings.join(" ")}`
+    }
+    
 }
 
 
-//funcao pra organizar as letras alfabeticamente
+
 function alphabetize(string) {
-    return string.toLowerCase().split("").sort().join("").trim();
+    return string.toLowerCase().split("").sort().join("").trim(); 
 }
+
